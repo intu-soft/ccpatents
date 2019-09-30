a8c96ebed7d3d362eb4eafdf6ad80061747e884d
joondong2 <kwonjoondong@gmail.com>
2019-10-01 00:43:18 +0900

    블로그 섹션 탬플릿 구현

--- js/creative.js
+++ js/creative.js
@@ -57,6 +57,30 @@
 
   $(".youtube-popup").grtyoutube();
 
+  $('.carousel-project').owlCarousel({
+    autoplay: true,
+    autoHeight: true,
+    center: true,
+    loop: true,
+    items:10,
+    margin: 30,
+    stagePadding: 0,
+    nav: false,
+    dots: true,
+    navText: ['<span class="ion-ios-arrow-back">', '<span class="ion-ios-arrow-forward">'],
+    responsive:{
+      0:{
+        items: 1
+      },
+      600:{
+        items: 2
+      },
+      1000:{
+        items: 3
+      }
+    }
+  });
+
 })(jQuery); // End of use strict
 
 $(window).resize(function () {
