1df24eff35ba25006702c0d582ae11186d0a4853
joondong2 <kwonjoondong@gmail.com>
2019-10-02 12:41:15 +0900

    스토어, 공유 버튼 설명 문구 모바일에서 크기 작아지게 변경

--- js/creative.js
+++ js/creative.js
@@ -14,6 +14,8 @@
     }
   }
 
+  document.getElementById("share-button").style.display = "block"; 
+
 
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
