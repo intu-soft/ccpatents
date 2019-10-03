e9251cb50a4b2386d4f8693727d26d7edd5bf2d0
joondong2 <kwonjoondong@gmail.com>
2019-10-03 10:34:07 +0900

    설치 방법 색션에서 step2 설명이 lg 크기 이상에서 오른쪽으로 정렬되도록 설정

--- js/form-submission-handler.js
+++ js/form-submission-handler.js
@@ -1,30 +1,30 @@
-(function() {
+(function () {
   // get all data in form and return object
   function getFormData(form) {
     var elements = form.elements;
     var honeypot;
 
-    var fields = Object.keys(elements).filter(function(k) {
+    var fields = Object.keys(elements).filter(function (k) {
       if (elements[k].name === "honeypot") {
         honeypot = elements[k].value;
         return false;
       }
       return true;
-    }).map(function(k) {
-      if(elements[k].name !== undefined) {
+    }).map(function (k) {
+      if (elements[k].name !== undefined) {
         return elements[k].name;
-      // special case for Edge's html collection
-      }else if(elements[k].length > 0){
+        // special case for Edge's html collection
+      } else if (elements[k].length > 0) {
         return elements[k].item(0).name;
       }
-    }).filter(function(item, pos, self) {
+    }).filter(function (item, pos, self) {
       return self.indexOf(item) == pos && item;
     });
 
     var formData = {};
-    fields.forEach(function(name){
+    fields.forEach(function (name) {
       var element = elements[name];
-      
+
       // singular form elements just have one value
       formData[name] = element.value;
 
@@ -44,14 +44,15 @@
     // add form-specific values into the data
     formData.formDataNameOrder = JSON.stringify(fields);
     formData.formGoogleSheetName = form.dataset.sheet || "responses"; // default sheet name
-    formData.formGoogleSend
-      = form.dataset.email || ""; // no email by default
+    formData.formGoogleSend = form.dataset.email || ""; // no email by default
 
-    return {data: formData};
+    return {
+      data: formData
+    };
   }
 
-  function handleFormSubmit(event) {  // handles form submit without any jquery
-    event.preventDefault();           // we are submitting via xhr below
+  function handleFormSubmit(event) { // handles form submit without any jquery
+    event.preventDefault(); // we are submitting via xhr below
     var form = event.target;
     var formData = getFormData(form);
     var data = formData.data;
@@ -67,27 +68,28 @@
     xhr.open('POST', url);
     // xhr.withCredentials = true;
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-          form.reset();
-          enableAllButtons(form);
-          /*var formElements = form.querySelector(".form-elements")
-          if (formElements) {
-            formElements.style.display = "none"; // hide form
-          }
-          var thankYouMessage = form.querySelector(".thankyou_message");
-          if (thankYouMessage) {
-            thankYouMessage.style.display = "block";
-          }*/
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        form.reset();
+        enableAllButtons(form);
+
+        /*var formElements = form.querySelector(".form-elements")
+        if (formElements) {
+          formElements.style.display = "none"; // hide form
         }
+        var thankYouMessage = form.querySelector(".thankyou_message");
+        if (thankYouMessage) {
+          thankYouMessage.style.display = "block";
+        }*/
+      }
     };
     // url encode form data for sending as post data
-    var encoded = Object.keys(data).map(function(k) {
-        return encodeURIComponent(k) + "=" + encodeURIComponent(data[k]);
+    var encoded = Object.keys(data).map(function (k) {
+      return encodeURIComponent(k) + "=" + encodeURIComponent(data[k]);
     }).join('&');
     xhr.send(encoded);
   }
-  
+
   function loaded() {
     // bind to the submit event of our form
     var forms = document.querySelectorAll("form.gform");
